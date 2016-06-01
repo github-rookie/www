26c57fd2dcef528250b8f63680c384b4ec7eea2b
lei jin <lei jin>
2016-06-01 10:22:53 +0800

    changes log

---
+++ a.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog v10.2 
+MySQL - 5.6.17 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `dk_apply_record` (
+	`id` int (11),
+	`uid` int (11),
+	`source` varchar (150),
+	`type` int (2),
+	`paas` varchar (150),
+	`status` varchar (150),
+	`adate` datetime 
+); 
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('1','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+
+
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
+insert into `dk_apply_record` (`id`, `uid`, `source`, `type`, `paas`, `status`, `adate`) values('','1','baidu','1','1,2,3','1,0,1','2016-05-18 23:53:32');
